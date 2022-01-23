018ed96719ab106c118070185683219102c04613
bamin0422 <bamin0422@gmail.com>
2022-01-23 17:12:31 +0900

    Refactor file tree

--- app/src/main/java/com/intention/android/goodmask/activity/DeviceActivity.kt
+++ app/src/main/java/com/intention/android/goodmask/activity/DeviceActivity.kt
@@ -1,12 +1,13 @@
-package com.intention.android.goodmask
+package com.intention.android.goodmask.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.intention.android.goodmask.R
 
-class SplashActivity : AppCompatActivity() {
+class DeviceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_main)
 
     }
 }
---
+++ app/src/main/java/com/intention/android/goodmask/fragment/MaskFragment.kt
@@ -0,0 +1,15 @@
+package com.intention.android.goodmask.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.intention.android.goodmask.R
+
+class MaskFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.frag_mask, container, false)
+    }
+}
